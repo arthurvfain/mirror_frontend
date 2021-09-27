abe54b0f07c9db142df820b0e807b35b543a2968
Arthur V Fain <arthurvfain@gmail.com>
2021-09-27 15:23:20 -0400

    less funny this time

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,10 +1,9 @@
-import logo from '../logo.svg';
 import '../App.css';
 import NavBar from './NavBar'
 import SignUp from './SignUp'
 import Login from './Login'
 import {useState, useEffect} from 'react'
-import {BrowserRouter, Switch, Route, Redirect} from 'react-router-dom'
+import {Switch, Route} from 'react-router-dom'
 
 
 function App() {
---
+++ src/Components/SignUp.js
@@ -0,0 +1,68 @@
+import {useState} from 'react'
+import {useHistory} from 'react-router-dom'
+import { Form, Button } from 'react-bootstrap'
+
+function SignUp ({setCurrentUser}) {
+    const [formData, setFormData] = useState({username: '', password: '', address: ''})
+    const [errors, setErrors] = useState([])
+
+    let history = useHistory()
+
+    function handleChange(e) {
+        let prop = e.target.name
+        let val = e.target.value
+
+        let newData = {
+            ...formData,
+            [prop]: val
+        }
+        setFormData(newData)
+    }
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+        let resp = await fetch('/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(formData)
+        })
+
+        if (resp.ok) {
+            setErrors([])
+            resp.json().then(user=>{
+                setCurrentUser(user)
+                history.push('/')
+            })
+            // TO DO: ADD REDIRECT
+        } else {
+            resp.json().then(data => setErrors(data.errors))
+        }
+    }
+
+    return (
+        <div className='pageContent'>
+            <Form onSubmit={handleSubmit} style={{maxWidth: '18rem', margin: 'auto'}}>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Username</Form.Label>
+                    <Form.Control type="text" placeholder="Username" onChange={handleChange} name='username' value={formData.username}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control type="text" placeholder="Email" onChange={handleChange} name='address' value={formData.address}/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control type="password" placeholder="Password" onChange={handleChange} name='password' value={formData.password}/>
+                </Form.Group>
+                <Button variant="primary" type="submit">
+                    Submit
+                </Button>
+            </Form>
+            {errors ? errors.map(error => <li>{error}</li>) : null}
+        </div>
+    )
+}
+
+export default SignUp
